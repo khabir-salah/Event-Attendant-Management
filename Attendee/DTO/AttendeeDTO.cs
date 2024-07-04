61907ee35ee5c8c3a4149f26365b69d236a97fa4
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-07-04 10:46:47 +0100

    using asynchronous coding

--- Attendee/DTO/AttendeeDTO.cs
+++ Attendee/DTO/AttendeeDTO.cs
@@ -1,14 +1,22 @@
 ﻿using Attendee.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Attendee.DTO
 {
     public class AttendeeDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "must fill"), MinLength(3, ErrorMessage = "more yhan 3 letters needed"), MaxLength(15, ErrorMessage = "less than 15")]
         public string? FirstName { get; set; }
+        [Required(ErrorMessage = "must fill"), MinLength(3, ErrorMessage = "more yhan 3 letters needed"), MaxLength(15, ErrorMessage = "less than 15")]
         public string? LastName { get; set; }
+        [Required]
         public byte Age { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{yyyy-mm-dd}", ApplyFormatInEditMode = true)]
         public DateTime ArrivalTime { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{yyyy-mm-dd}", ApplyFormatInEditMode = true)]
         public DateTime DepartureTime { get; set; }
         public bool IsCheckedOut { get; set; }
         public Status IsSteppedOut { get; set; }
--- Attendee/Entities/Attendant.cs
+++ Attendee/Entities/Attendant.cs
@@ -1,27 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
-
+﻿
 namespace Attendee.Entities
 {
     public class Attendant
     {
-
         public int Id { get; set; }
-        [Required(ErrorMessage = "must fill"), MinLength(3, ErrorMessage = "more yhan 3 letters needed"), MaxLength(15, ErrorMessage = "less than 15")]
         public string? FirstName { get; set; }
-        [Required(ErrorMessage = "must fill"), MinLength(3, ErrorMessage = "more yhan 3 letters needed"), MaxLength(15, ErrorMessage = "less than 15")]
         public string? LastName { get; set; }
-        [Required]
         public byte Age { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{yyyy-mm-dd}", ApplyFormatInEditMode = true)]
         public DateTime ArrivalTime { get; set; }
-        [DataType(DataType.Date)]
-        [ DisplayFormat(DataFormatString = "{yyyy-mm-dd}", ApplyFormatInEditMode = true)]
         public DateTime DepartureTime { get; set; }
         public bool IsCheckedOut { get; set; }
         public Status IsSteppedOut { get; set; }
-        
+       
     }
 
     public class Event
@@ -36,4 +26,6 @@ namespace Attendee.Entities
         Outside,
         Returned
     }
+
+   
 }
